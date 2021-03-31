6349e7d2b5f803ec1110b8b8edeb4597b1936b46
Stephan Lensky <stephanl.public@gmail.com>
2021-03-31 16:39:58 -0400

    Working weka demo @ 77% accuracy

--- src/main/java/edu/umass/wekademo/WekaDemo.java
+++ src/main/java/edu/umass/wekademo/WekaDemo.java
@@ -1,9 +1,35 @@
 package edu.umass.wekademo;
 
+import weka.classifiers.Evaluation;
+import weka.classifiers.trees.J48;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+import java.util.Random;
+
 public class WekaDemo {
 
-    public static void main(String[] args) {
-        System.out.println("Hello gradle!");
+    public static void main(String[] args) throws Exception {
+        // code from
+        // https://waikato.github.io/weka-wiki/use_weka_in_your_java_code/
+
+        // I encountered a weka bug using csv files, so I converted the dataset to arff
+        DataSource source = new DataSource("heart.arff");
+        Instances data = source.getDataSet();
+        data.setClassIndex(data.numAttributes() - 1);
+
+        String[] options = new String[1];
+        options[0] = "-U";            // unpruned tree
+        J48 tree = new J48();         // new instance of tree
+        tree.setOptions(options);     // set the options
+
+        // call to buildClassifier() is apparently not necessary when using Evaluation, since Evaluation handles training
+//        tree.buildClassifier(data);   // build classifier
+
+        Evaluation eval = new Evaluation(data);
+        eval.crossValidateModel(tree, data, 10, new Random(1));
+        System.out.println(eval.correct());
+        System.out.println(eval.pctCorrect());
     }
 
 }
